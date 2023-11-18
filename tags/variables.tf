cc5c41e419321569c888c8ea84de2587ec3ec0f9
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2023-11-18 01:01:33 -0300

    feat(tags): Tags module creation

---
+++ tags/variables.tf
@@ -0,0 +1,24 @@
+variable "environment" {
+  type        = string
+  description = "The resource's environment"
+}
+
+variable "application" {
+  type        = string
+  description = "The application that owns the resource"
+}
+
+variable "team" {
+  type        = string
+  description = "The squad that owns the resource"
+}
+
+variable "aws_object" {
+  type        = string
+  description = "he resource identifier, ex: ec2, s3, rds, etc..."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the resource"
+}
