c7d7ae1bcca360e47913f818764f8d7cd0af8b05
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2023-11-20 16:24:20 -0300

    fix: [SQS] aws_object requred tag fix

--- modules/sqs/variables.tf
+++ modules/sqs/variables.tf
@@ -13,11 +13,6 @@ variable "team" {
   description = "The squad that owns the resource"
 }
 
-variable "aws_object" {
-  type        = string
-  description = "he resource identifier, ex: ec2, s3, rds, etc..."
-}
-
 variable "name" {
   type        = string
   description = "The name of the resource"
