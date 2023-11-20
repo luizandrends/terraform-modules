f7c8d911307b62db60fede947a529636733dc321
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2023-11-19 23:30:27 -0300

    feat: [VPC] Creating VPC resource and Default SG

---
+++ modules/vpc/main.tf
@@ -0,0 +1,64 @@
+locals {
+  create_vpc = var.create_vpc
+}
+
+module "tags" {
+  source = "git@github.com:luizandrends/terraform-modules.git//tags?ref=v1.5.0"
+
+  name            = var.name
+  environment     = var.environment
+  application     = var.application
+  team            = var.team
+  aws_object      = "vpc"
+  additional_tags = var.additional_tags
+}
+
+resource "aws_vpc" "this" {
+  count = local.create_vpc ? 1 : 0
+
+  cidr_block = var.cidr
+
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+
+  tags = module.tags.default_tags
+}
+
+resource "aws_default_security_group" "this" {
+  count = local.create_vpc ? 1 : 0
+
+  vpc_id = aws_vpc.this[0].id
+
+  dynamic "ingress" {
+    for_each = var.default_security_group_ingress
+    content {
+      self             = lookup(ingress.value, "self", null)
+      cidr_blocks      = compact(split(",", lookup(ingress.value, "cidr_blocks", "")))
+      ipv6_cidr_blocks = compact(split(",", lookup(ingress.value, "ipv6_cidr_blocks", "")))
+      prefix_list_ids  = compact(split(",", lookup(ingress.value, "prefix_list_ids", "")))
+      security_groups  = compact(split(",", lookup(ingress.value, "security_groups", "")))
+      description      = lookup(ingress.value, "description", null)
+      from_port        = lookup(ingress.value, "from_port", 0)
+      to_port          = lookup(ingress.value, "to_port", 0)
+      protocol         = lookup(ingress.value, "protocol", "-1")
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.default_security_group_egress
+    content {
+      self             = lookup(egress.value, "self", null)
+      cidr_blocks      = compact(split(",", lookup(egress.value, "cidr_blocks", "")))
+      ipv6_cidr_blocks = compact(split(",", lookup(egress.value, "ipv6_cidr_blocks", "")))
+      prefix_list_ids  = compact(split(",", lookup(egress.value, "prefix_list_ids", "")))
+      security_groups  = compact(split(",", lookup(egress.value, "security_groups", "")))
+      description      = lookup(egress.value, "description", null)
+      from_port        = lookup(egress.value, "from_port", 0)
+      to_port          = lookup(egress.value, "to_port", 0)
+      protocol         = lookup(egress.value, "protocol", "-1")
+    }
+  }
+
+  tags = module.tags.default_tags
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,78 @@
+variable "environment" {
+  type        = string
+  description = "The resource's environment"
+}
+
+variable "application" {
+  type        = string
+  description = "The application that owns the resource"
+}
+
+variable "team" {
+  type        = string
+  description = "The squad that owns the resource"
+}
+
+variable "aws_object" {
+  type        = string
+  description = "he resource identifier, ex: ec2, s3, rds, etc..."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the resource"
+}
+
+variable "additional_tags" {
+  type        = map(any)
+  default     = {}
+  description = "A map containing additional tags"
+}
+
+variable "create_vpc" {
+  description = "Controls if VPC should be created (it affects almost all resources)"
+  type        = bool
+  default     = true
+}
+
+variable "cidr" {
+  description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "ipv4_netmask_length" {
+  description = "(Optional) The netmask length of the IPv4 CIDR you want to allocate to this VPC. Requires specifying a ipv4_ipam_pool_id"
+  type        = number
+  default     = null
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  type        = string
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "default_security_group_ingress" {
+  description = "List of maps of ingress rules to set on the default security group"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "default_security_group_egress" {
+  description = "List of maps of egress rules to set on the default security group"
+  type        = list(map(string))
+  default     = []
+}
